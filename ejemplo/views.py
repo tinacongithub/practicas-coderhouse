f1e04113cd65de03152c638cc516d5c69e81323c
Tanya Valentina Mendez P <tvalenm@gmail.com>
2022-12-04 21:30:48 -0300

    MVT+Mendez Entregable

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from ejemplo.models import Familiar
 
 def index(request):
     return render(request, "ejemplo/saludar.html")
@@ -9,3 +10,19 @@ def saludar_a(request, nombre):
     'ejemplo/saludar_a.html',
     {'nombre': nombre}
     )
+
+def buscar(request):
+    lista_de_nombres = ["Merlina", "Pericles", "Morticia", "Homero"]
+    query = request.GET['q']
+   
+    if query in lista_de_nombres:
+        indice_de_resultado = lista_de_nombres.index(query)
+        resultado = lista_de_nombres[indice_de_resultado]
+    else:
+        resultado = "no está en la lista"
+        
+    return render(request, 'ejemplo/buscar.html', {"resultado": resultado})
+
+def mostrar_familiares(request):
+  lista_familiares = Familiar.objects.all()
+  return render(request, "ejemplo/familiares.html", {"lista_familiares": lista_familiares}) 
--- project/urls.py
+++ project/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ejemplo.views import index, saludar_a
+from ejemplo.views import index, saludar_a, buscar, mostrar_familiares
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludar/', index),
     path('saludar-a/<nombre>/', saludar_a),
+    path('buscar/', buscar),
+    path('Familia-Addams/', mostrar_familiares),
 ]
