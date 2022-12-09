25bf154c00fa78e96971d9bd007a70d2d5ee0853
Tanya Valentina Mendez P <tvalenm@gmail.com>
2022-12-09 18:43:44 -0300

    agrego archivo seed_data

---
+++ seed_data.py
@@ -0,0 +1,6 @@
+from ejemplo.models import Familiar
+Familiar(nombre="Cosa", direccion="Salem 666", numero_pasaporte=123123).save()
+Familiar(nombre="Dedos", direccion="Salem 666", numero_pasaporte=890890).save()
+Familiar(nombre="Largo", direccion="Salem 666", numero_pasaporte=345345).save()
+Familiar(nombre="Lucas", direccion="Salem 666", numero_pasaporte=567567).save()
+print("Se cargo con éxito los usuarios de pruebas")
